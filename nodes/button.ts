5c63799c321b6dc5163fd64a8380690102dd5065
Den Williams <dennis.williams@loke.com.au>
2016-09-28 22:57:31 +1000

    added button node

---
+++ nodes/button.ts
@@ -0,0 +1,27 @@
+import { IEventBus } from 'homenet-core';
+
+export = function(RED) {
+  const global = RED.settings.functionGlobalContext;
+  const eventBus: IEventBus = global.services.get('IEventBus');
+
+  function NodeIn(config) {
+    const node = this;
+    RED.nodes.createNode(node, config);
+    const evt = `button.${config.button}.${config.action}`;
+
+    eventBus.on(evt, null, handleEvent);
+
+    node.on('close', () => {
+      eventBus.removeListener(evt, null, handleEvent);
+    });
+
+    function handleEvent(e) {
+      node.send({
+        topic: `button/${config.button}/${config.action}`,
+        payload: e.data
+      });
+    }
+  }
+
+  RED.nodes.registerType("appevent in", NodeIn);
+};
