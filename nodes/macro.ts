de273b2c57c8da686fb762610f36d22d82b8a6e6
Den Williams <work@denwilliams.net>
2017-07-30 11:56:29 +1000

    macro execute

--- nodes/macro.ts
+++ nodes/macro.ts
@@ -1,8 +1,10 @@
+import { ICommandManager } from './../homenet-core.d';
 import { IEventBus } from '@homenet/core';
 
 export = function(RED) {
   const global = RED.settings.functionGlobalContext;
   const eventBus: IEventBus = global.services.get('IEventBus');
+  const commands: ICommandManager = global.services.get('ICommandManager');
 
   function MacroNode(config) {
     const node = this;
@@ -23,5 +25,16 @@ export = function(RED) {
     }
   }
 
+  function ExecuteMacroNode(config) {
+    const node = this;
+    RED.nodes.createNode(node, config);
+
+    node.on('input', function(msg) {
+      var args = msg.payload;
+      commands.run(`macro.${config.macro}`, 'execute', args);
+    });
+  }
+
   RED.nodes.registerType("macro", MacroNode);
+  RED.nodes.registerType("macro-exec", ExecuteMacroNode);
 };
