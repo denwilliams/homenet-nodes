3c36615f9cb971007a5de4f6fdd5f5aac0b759b5
Den Williams <work@denwilliams.net>
2018-04-16 22:04:07 +1000

    expected value

---
+++ nodes/isvalue.ts
@@ -0,0 +1,21 @@
+export = function(RED) {
+  function Node(config) {
+    const node = this;
+
+    RED.nodes.createNode(this, config);
+
+    const { valueId, key, expectedValue } = config;
+    const checkValue = JSON.parse(expectedValue);
+
+    var values = RED.settings.functionGlobalContext.services.get('IValueManager');
+
+    this.on('input', function(msg) {
+      const currentValue = values.get(valueId, null, key);
+
+      if (currentValue === checkValue) node.send([msg,null]);
+      else node.send([null,msg]);
+    });
+  }
+
+  RED.nodes.registerType('isvalue', Node);
+};
